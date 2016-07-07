6c9470efcea06561cb0ce6ac5010b60b5352edb3
Nandhinikandhan <kandhannandhini@gmail.com>
2016-07-07 22:54:22 +0530

    Update and rename .gitignore to .gitignore.c

---
+++ .gitignore.c
@@ -0,0 +1,12 @@
+int main() { int a; 
+clrscr();
+printf("enter the number");
+scanf("%d",&a);
+if(a<0)
+printf("negative");
+else if(a>0)
+printf("positive");
+else
+printf("zero");
+getch(); 
+return 0; }
